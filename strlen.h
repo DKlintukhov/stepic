f05c1ebfc7f3b120d02a5c478dcdfc165fc94fdc
Denis Klintukhov <denis.klintukhov@gmail.com>
2023-08-30 18:56:21 +0300

    strstr

--- strlen.h
+++ strlen.h
@@ -16,7 +16,38 @@ namespace mystrlen
         return len;
     }
 
-    void tests() {
+    int mystrstr(const char *text, const char *pattern)
+    {
+        if (*pattern == '\0')
+            return 0;
+        if (mystrlen(text) < mystrlen(pattern))
+            return -1;
+
+        int position = 0;
+
+        while (*text != '\0')
+        {
+            if (*text == *pattern)
+            {
+                int textStep = 0;
+                int patternStep = 0;
+                while (*(text + textStep) == *(pattern + patternStep))
+                {
+                    textStep++;
+                    patternStep++;
+                    if (*(pattern + patternStep) == '\0')
+                        return position;
+                }
+            };
+            text++;
+            position++;
+        }
+
+        return -1;
+    }
+
+    void tests()
+    {
         assert(mystrlen("Hello") == 5);
     }
 }
